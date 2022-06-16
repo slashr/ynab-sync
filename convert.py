a7dc8e233b477bd10e6d204d6426832358f36447
slashr <slashr@users.noreply.github.com>
2022-06-16 20:03:49 +0200

    Rename columns

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 f = pd.read_csv("n26.csv")
 
-keep_col = ['Date','Payee','Payment reference','Amount (EUR)']
-new_f = f[keep_col]
-new_f.to_csv("new.csv", index=False)
+keep_columns = ['Date','Payee','Payment reference','Amount (EUR)']
+f2 = f[keep_columns]
+f3 = f2.rename(columns = {'Payment reference': 'Memo', 'Amount (EUR)': 'Amount'})
+f3.to_csv("new.csv", index=False)

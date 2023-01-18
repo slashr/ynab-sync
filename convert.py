3417c9b4c4cf25d2a6a546893d21a9a2c385ebef
slashr <slashr@users.noreply.github.com>
2023-01-18 17:02:34 +0530

    Update

--- convert.py
+++ convert.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import os
 
-f = pd.read_csv("n26.csv")
+f = pd.read_csv("/Users/primus/Downloads/n26-csv-transactions.csv")
 
 keep_columns = ['Date','Payee','Payment reference','Amount (EUR)']
 f2 = f[keep_columns]
 f3 = f2.rename(columns = {'Payment reference': 'Memo', 'Amount (EUR)': 'Amount'})
-f3.to_csv("new.csv", index=False)
+f3.to_csv("new.csv", date_format='%Y%m%d')
+
+os.remove("/Users/primus/Downloads/n26-csv-transactions.csv") 
